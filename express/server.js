3155d37113f784230121af1c8058d220efd40a40
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2020-04-02 22:50:33 +0200

    deploy test

--- express/server.js
+++ express/server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js test1!</h1>');
+  res.write('<h1> dasdqasds Hello from Express.js test1!</h1>');
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
