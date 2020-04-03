a7cc6235a16a6d7b40bb92ccb1fe8307f201b01c
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2020-04-03 20:40:49 +0200

    rm sl

--- express/server.js
+++ express/server.js
@@ -6,12 +6,17 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1> dasdqasds Hello from Express.js test1!</h1>');
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-
-router.get('/hello', (req, res) => res.send({ test: 'hello222'}));
-
-router.get('/another', (req, res) => res.send({ route: 'hello2' }));
+app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
