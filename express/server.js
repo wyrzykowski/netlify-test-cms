ee5fcd7ecf3fb03dcb191e2557a6d78ad6167680
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2020-04-03 21:28:06 +0200

    ro

--- express/server.js
+++ express/server.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const serverless = require("serverless-http");
-
+'use strict';
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
 const app = express();
+const bodyParser = require('body-parser');
+
 const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
+router.get('/', (req, res) => {
+res.send('heheh');
 });
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
-app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
