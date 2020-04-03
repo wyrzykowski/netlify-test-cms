41b772c9ff691c46bb561c37b2d8fbe04ccad7c1
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2020-04-03 21:08:51 +0200

    5

--- express/server.js
+++ express/server.js
@@ -1,22 +1,14 @@
 'use strict';
 const express = require('express');
-const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1> dasdqasds Hello from Express.js test1!</h1>');
-  res.end();
+
+
+app.use(bodyParser);
+app.get('/hello', (req, res) => {
+  res.json('hello');
 });
-router.get('/another', (req, res) => res.send({ route: 'hell0 another' }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use(router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-
-module.exports = app;
 module.exports.handler = serverless(app);
