7fb710348a1c1610fdde37783083df9a46f22fe6
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2020-04-03 20:25:20 +0200

    5

--- express/server.js
+++ express/server.js
@@ -1,21 +1,21 @@
 'use strict';
 const express = require('express');
 const path = require('path');
+const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1> dasdqasds Hello from Express.js test2!</h1>');
+  res.write('<h1> dasdqasds Hello from Express.js test1!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.send('Hello');
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ test: 'hello'}));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
-
