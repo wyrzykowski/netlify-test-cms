79fb4401432941a9a4b510f167ed3b5ab1a6f176
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2020-04-03 20:33:15 +0200

    rm sl

--- express/server.js
+++ express/server.js
@@ -1,7 +1,6 @@
 'use strict';
 const express = require('express');
 const path = require('path');
-const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -15,4 +14,3 @@ router.get('/another', (req, res) => res.send({ route: 'hello2' }));
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
-module.exports.handler = serverless(app);
