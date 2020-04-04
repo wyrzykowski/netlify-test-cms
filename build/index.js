bbd2c289fffa24bc238095b43c3b0a5cd003a6c5
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2020-04-04 16:43:21 +0200

    dasd

--- build/index.js
+++ build/index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 
 router.get('/hello', (req, res) => {
-  res.send('hello test 04 build express');
+  res.send('hello test 04 build index');
   });
 
   router.get('/hello2', (req, res) => {
